28202868bffc1e68f671afe76cb0004146add64b
Aivaras <lukauskasaivaras@gmail.com>
2019-12-16 13:55:32 +0100

    generateRange(min, max, step)

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+//*******************************************************************************************
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step.The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+//**************************************************************************************** */
+
+function generateRange(min, max, step) {
+    newRange = [];
+    for (let i = min; i <= max; i += step) {
+        newRange.push(i)
+    }
+    return newRange;
+}
+
+generateRange(2, 19, 3);
+
+//**************************************************************************************** */
